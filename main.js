42d588b37d2ccc08be4022c0730a68b985037539
vadymdk <vadim.dk81@gmail.com>
2019-03-13 22:55:32 +0200

    task4

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-//  Завдання 1 A function which prints str after n seconds.
+       //  Завдання 1 A function which prints str after n seconds.
 
 
 //setTimeout(function() { console.log('hello') }, 10000);
 
-    //Завдання 2 A function which returns sum of all numbers from 1 to n using recursion
+      //Завдання 2 A function which returns sum of all numbers from 1 to n using recursion
 
 //function sumAll(n) { 
 //if (n == 1) return 1;
@@ -15,7 +15,7 @@
 //console.log(sumAll(4)); // 10
 
 
-// Завдання 3     A function which takes str and time in seconds as arguments, then every second it should count down from time to 0 and print current time to console when time equals to 0 it prints str
+      // Завдання 3     A function which takes str and time in seconds as arguments, then every second it should count down from time to 0 and print current time to console when time equals to 0 it prints str
 
 //function bombTimer(str,time) {
 //  let i = time;
@@ -30,4 +30,19 @@
 //}
 //
 //bombTimer('Boooom', 3);
+
+
+     // Завдання 4  A function which returns factorial of number using recursion 
+
+//function factorial(n) {
+//  if(n != 1){
+//    return n * factorial(n - 1); 
+//  }   
+//    else {
+//        return 1;
+//    }
+//}
+//
+//console.log( factorial(5) );
+//console.log( factorial(3) );
 
