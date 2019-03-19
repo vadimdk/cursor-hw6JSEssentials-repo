d652ba65cdd8df59b5bb0cb209b17752d2f6089c
vadymdk <vadim.dk81@gmail.com>
2019-03-19 21:27:17 +0200

    task6

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
        //  Завдання 1 A function which prints str after n seconds.
 
 
-//setTimeout(function() { console.log('hello') }, 10000);
+const strHello = setTimeout(function() { console.log('hello') }, 10000);
 
       //Завдання 2 A function which returns sum of all numbers from 1 to n using recursion
 
@@ -17,7 +17,7 @@ if (n == 1) return 1;
 
       // Завдання 3     A function which takes str and time in seconds as arguments, then every second it should count down from time to 0 and print current time to console when time equals to 0 it prints str
 
-function bombTimer(str,time) {
+const bombTimer = function(str,time) {
   let i = time;
   let timerId = setTimeout(function go() {
     console.log(i);
@@ -25,35 +25,43 @@ function bombTimer(str,time) {
     i--;
   }, 1000);
     let printStr = setTimeout(function print() {
-        console.log('Boooom')},1600*time);  
+        console.log('')},1600*time);  
     
 }
 
-//bombTimer('Boooom', 3);
 
 
 
 
-//function backCount(n) {
-//    console.log(n);
-//      if (n == 0) return 0;
-//  return backCount(n - 1);
-//}
-//
-// backCount(10);
+
+
 
 
      // Завдання 4  A function which returns factorial of number using recursion 
 
-function factorial(n) {
+const factorial = function(n) {
   if(n != 1){
     return n * factorial(n - 1); 
   }   
     else {
         return 1;
     }
-//}
-//
-//console.log( factorial(5) );
-//console.log( factorial(3) );
+}
+
+
+
+//    Завдання 6. A function which takes an array of numbers and maxNumber, the function returns new array with numbers not higher than maxNumber.
+
+
+const filterNumbers = function (arr, maxNumber) { 
+ 
+    const newArr = arr.filter(chekingFunction);
+
+   function chekingFunction(value) {
+       return value < maxNumber;
+   }
+return newArr;
+}
+filterNumbers([1, 4, 8, 1, 20], 5) // [1, 4, 1]
+
 
